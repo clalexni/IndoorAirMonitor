42671c6b67036129cd6301e871db510808674294
jessedadams <53625810+jessedadams@users.noreply.github.com>
2020-09-24 10:30:53 -0500

    Update OutputParser.kt

--- temp/OutputParser.kt
+++ temp/OutputParser.kt
@@ -3,7 +3,7 @@ class OutputParser (val value1: Float?,
 
     enum class PeakFlowRateValue { GREEN, YELLOW, RED }
 
-    getPeakFlowValueRate(val1: Float, val2: Float) {
+    getPeakFlowValueRate(val1: Float, val2: Float): PeakFlowValueRate {
         val peakFlowValueRate: PeakFlowRateValue = {
             /*
             * Code to translate ML data to alert
