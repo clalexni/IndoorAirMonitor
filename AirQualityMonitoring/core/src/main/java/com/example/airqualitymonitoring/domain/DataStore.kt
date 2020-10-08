07caca2f4b1dc844a238ff16d678236d00f816d5
Jesse Adams <jda170001@utdallas.edu>
2020-10-08 10:29:45 -0500

    Third Domain update - 10/08/2020

--- AirQualityMonitoring/core/src/main/java/com/example/airqualitymonitoring/domain/DataStore.kt
+++ AirQualityMonitoring/core/src/main/java/com/example/airqualitymonitoring/domain/DataStore.kt
@@ -2,7 +2,7 @@ package com.example.airqualitymonitoring.domain
 
 import java.time.LocalDateTime
 
-data class DataArray(val airQuality: AirQuality,
+data class DataStore(val airQuality: AirQuality,
                      val peakFlow: PeakFlow,
                      val questionnaire: Questionnaire,
                      val weather: Weather) {
