6dfab455b054f788d79a9802efcc2a911eb1bb77
Huzai <huzaifa.m.s@outlook.com>
2020-10-13 08:45:42 -0500

    Changed Domain AirQuality.kt.
    Added Device Name and Pm 2.5 and Pm 10.0

--- AirQualityMonitor/core/src/main/java/com/example/airqualitymonitoring/domain/AirQuality.kt
+++ AirQualityMonitor/core/src/main/java/com/example/airqualitymonitoring/domain/AirQuality.kt
@@ -1,4 +1,4 @@
 package com.example.airqualitymonitoring.domain
 
-data class AirQuality(val airQuality: Float) {
+data class AirQuality(val deviceID: String, val pm2_5: Double, val pm10_0: Double) {
 }
