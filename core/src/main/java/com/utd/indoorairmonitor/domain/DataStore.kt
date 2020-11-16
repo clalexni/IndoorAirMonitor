f3752c557bcb28d9223a7e1ff163ce7b9a2a5e8f
clalexni <cl.alexni@gmail.com>
2020-11-15 23:25:38 -0600

    modify core related to questionnaire and peakflow

--- core/src/main/java/com/utd/indoorairmonitor/domain/DataStore.kt
+++ core/src/main/java/com/utd/indoorairmonitor/domain/DataStore.kt
@@ -9,4 +9,6 @@ data class DataStore(val airMonitor: AirMonitor,
     //wasn't working because it requires API 26 and above
     //val timeStamp = LocalDateTime.now()
 
+    // API id now higher than 26 so would prob work
+
 }
